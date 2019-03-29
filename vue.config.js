cad15f0665ff5ed97bcd066d3ab187f1f590defb
Reiss <reiss.cashmore@outlook.com>
2019-03-29 11:19:44 +0000

    Added webpack config for inline <docs> blocks in SFCs

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,11 @@ module.exports = {
     config.module.rules.push({
       resourceQuery: /blockType=story/,
       loader: 'vue-storybook'
-    })
+    });
+    config.module.rules.push({
+      resourceQuery: /blockType=docs/,
+      use: ['storybook-readme/vue/docs-loader', 'html-loader', 'markdown-loader'],
+    });
+    
   }
 }
